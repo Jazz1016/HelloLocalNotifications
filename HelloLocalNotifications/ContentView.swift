cab5e3adcb2cb69585ab801fc9441995f6b479ca
James Lea <james.q.lea@gmail.com>
2023-05-31 10:36:31 -0600

    finished tutorial for basic Notifications in my swiftUI app

--- HelloLocalNotifications/ContentView.swift
+++ HelloLocalNotifications/ContentView.swift
@@ -7,15 +7,77 @@
 
 import SwiftUI
 
+enum NotificationAction: String {
+    case dismiss
+    case reminder
+}
+
+enum NotificationCategory: String {
+    case general
+}
+
+class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        let userInfo = response.notification.request.content.userInfo
+        print(userInfo)
+        
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .badge])
+    }
+    
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Button("Schedule Notification") {
+                let center = UNUserNotificationCenter.current()
+                
+                //Create content
+                let content = UNMutableNotificationContent()
+                content.title = "Hot Coffee"
+                content.body = "Your delicious coffee is ready!"
+                content.categoryIdentifier = NotificationCategory.general.rawValue
+                content.userInfo = ["customData": "Some Data"]
+                
+                if let url = Bundle.main.url(forResource: "coffee2", withExtension: "jpeg") {
+                    if let attachment = try? UNNotificationAttachment(identifier: "image", url: url) {
+                        content.attachments = [attachment]
+                    }
+                }
+                
+                //Create trigger
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+                
+                //Create request
+                let request = UNNotificationRequest(identifier: "goy", content: content, trigger: trigger)
+                
+                //Define actions
+                let dismissAction = UNNotificationAction(identifier: NotificationAction.dismiss.rawValue, title: "Dismiss", options: [])
+                
+                let reminderAction = UNNotificationAction(identifier: NotificationAction.reminder.rawValue, title: "Reminder", options: [])
+                
+                let generalCategory = UNNotificationCategory(identifier: NotificationCategory.general.rawValue, actions: [dismissAction, reminderAction], intentIdentifiers: [], options: [])
+                
+                //Set notification categories
+                center.setNotificationCategories([generalCategory])
+                
+                //Add request to notification center
+                center.add(request) { error in
+                    if let error = error {
+                        print(error)
+                    }
+                }
+                
+            }
+            
+            
         }
-        .padding()
     }
 }
 
--- HelloLocalNotifications/HelloLocalNotificationsApp.swift
+++ HelloLocalNotifications/HelloLocalNotificationsApp.swift
@@ -9,6 +9,19 @@ import SwiftUI
 
 @main
 struct HelloLocalNotificationsApp: App {
+    
+    private var delegate: NotificationDelegate = NotificationDelegate()
+    
+    init() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = delegate
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { result, error in
+            if let error = error {
+                print(error)
+            }
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
