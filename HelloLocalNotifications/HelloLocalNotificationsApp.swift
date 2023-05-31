ce83218b9ffdb92c1b1f81ad5b70b9f342e19fbd
James Lea <james.q.lea@gmail.com>
2023-05-31 08:55:05 -0600

    first commit

---
+++ HelloLocalNotifications/HelloLocalNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloLocalNotificationsApp.swift
+//  HelloLocalNotifications
+//
+//  Created by James Lea on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloLocalNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
